36dafe4f45f95c9089590a2706f4c3806e9e5727
berobbb <shashwatdimri5@gmail.com>
2024-12-14 10:53:56 +0530

    added genres

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -7,24 +7,28 @@ export default function MovieList() {
           name: "Inception",
           rating: 8.8,
           image: "/images/Inception.png",
+          genres:["Action","Drama"],
         },
         {
           id: 2,
           name: "The Dark Knight",
           rating: 9.0,
           image: "/images/Dark.png",
+          genres:["Action","Drama"],
         },
         {
           id: 3,
           name: "Interstellar",
           rating: 8.6,
           image: "/images/Interstellar.png",
+          genres:["Action","Drama"],
         },
         {
           id: 4,
           name: "Titanic",
           rating: 7.8,
           image: "/images/titanic.png",
+          genres:["Action","Drama"],
         },
       ];
   return (
